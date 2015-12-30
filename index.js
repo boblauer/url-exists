f3c50f50282988e74e67c376d23cdf670b247b17
Bob Lauer <rlauer@gmail.com>
2015-12-30 15:10:16 -0600

    HEAD request, not GET

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var https = require('https');
 var parse = require('url').parse;
 
 function urlExists(url, cb) {
-  var request = (parse(url).protocol === 'https:' ? https : http).request;
-  var req = request(url, function (res) {
+  var urlParts = parse(url);
+  var request = (urlParts.protocol === 'https:' ? https : http).request;
+
+  urlParts.method = 'HEAD';
+  var req = request(urlParts, function (res) {
     var found = /4\d\d/.test(res.statusCode) === false;
     cb(null, found);
   });
